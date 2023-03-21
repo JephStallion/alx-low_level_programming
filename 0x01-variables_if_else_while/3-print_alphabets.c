6cdfbc2016c79ee936a770385c639fe0da11ec30
JephStallion <jephthahu@gmail.com>
2023-03-20 17:01:27 -0700

    3 prints of the alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,16 +3,17 @@
 /**
  * main - Prints the alphabet
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
+
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-int i;
+	char i;
 
-for (i = 0; i < 52; i++)
-putchar(alp[i]);
-}
-putchar('\n');
-return (0);
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
 }
